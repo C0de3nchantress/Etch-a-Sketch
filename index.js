002e6c6a03aa302d75aef6cf9599ef5634ec7684
C0de3nchantress <dhruvdudeja999@gmail.com>
2024-10-07 23:35:47 +0530

    fixed the grid bug and now it's perfect, also added a heading!

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const container = document.querySelector(".grid.container");
+const gridContainer = document.querySelector(".grid.container");
 
-for (let i = 0; i < 128; i++) {
-    for (let j = 0; j < 128; j++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid-obj");
-        container.appendChild(grid);
+function createGrid(size) {
+    const gridSize = 500 / size;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const grid = document.createElement("div");
+            grid.style.width = `${gridSize}px`;
+            grid.style.height = `${gridSize}px`;
+            grid.classList.add("grid-obj");
+            gridContainer.appendChild(grid);
+        }
     }
+
+    const gridObjects = document.querySelectorAll(".grid-obj");
+    gridObjects.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.classList.add("perma-hover");
+        });
+    });
 }
 
-
-const gridObjects = document.querySelectorAll(".grid-obj");
-gridObjects.forEach(element => {
-    element.addEventListener("mouseover", () => {
-        element.classList.add("perma-hover");
-    });
-});
+createGrid(16);
