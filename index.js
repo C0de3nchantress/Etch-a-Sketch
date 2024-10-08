b4278ba58524d521e1f31cd7d767f80a89b91175
C0de3nchantress <dhruvdudeja999@gmail.com>
2024-10-08 23:15:06 +0530

    added functinality to click and drag to color rather than just hover

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const gridContainer = document.querySelector(".grid.container");
 
 function createGrid(size) {
-    // const gridSize = 500 / size;
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("grid-row");
 
         for (let j = 0; j < size; j++) {
             const grid = document.createElement("div");
-            // grid.style.width = `${gridSize}px`;
-            // grid.style.height = `${gridSize}px`;
             grid.classList.add("grid-obj");
             row.appendChild(grid);
         }
@@ -17,12 +14,25 @@ function createGrid(size) {
         gridContainer.appendChild(row);
     }
 
+    let mouseIsDown = false
     const gridObjects = document.querySelectorAll(".grid-obj");
     gridObjects.forEach((element) => {
-        element.addEventListener("mouseover", () => {
-            element.classList.add("perma-hover");
+        element.addEventListener("mousedown", () => {
+            mouseIsDown = true;
+            element.classList.toggle("perma-color");
         });
+
+        element.addEventListener("mouseover", () => {
+            if (mouseIsDown) {
+                element.classList.add("perma-hover");
+            }
+        })
+
+        element.addEventListener("mouseup", () => {
+            mouseIsDown = false;
+        })
     });
+    
 }
 
 createGrid(16);
@@ -30,7 +40,6 @@ const sliderValDiv = document.createElement("div");
 const sliderContainer = document.querySelector(".slider-container");
 
 sliderContainer.append(sliderValDiv);
-
 
 const sizeSlider = document.querySelector(".size-slider");
 sliderValDiv.textContent = `${sizeSlider.value}x${sizeSlider.value}`;
