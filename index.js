aaf6e54467ffe123c0cfc112647f29e553a20899
C0de3nchantress <dhruvdudeja999@gmail.com>
2024-10-08 22:28:08 +0530

    bug fix: implemented the grid with flexbox

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const gridContainer = document.querySelector(".grid.container");
 
 function createGrid(size) {
-    const gridSize = 500 / size;
+    // const gridSize = 500 / size;
     for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+
         for (let j = 0; j < size; j++) {
             const grid = document.createElement("div");
-            grid.style.width = `${gridSize}px`;
-            grid.style.height = `${gridSize}px`;
+            // grid.style.width = `${gridSize}px`;
+            // grid.style.height = `${gridSize}px`;
             grid.classList.add("grid-obj");
-            gridContainer.appendChild(grid);
+            row.appendChild(grid);
         }
+
+        gridContainer.appendChild(row);
     }
 
     const gridObjects = document.querySelectorAll(".grid-obj");
